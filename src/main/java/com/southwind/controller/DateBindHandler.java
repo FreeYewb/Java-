7ec7ad0e43b081e3f95595dbc6ee29edaf157cb5
yewenbo <yewenbo@tienon.com>
2019-11-29 18:20:38 +0800

    配置5.0.11jar包

--- src/main/java/com/southwind/controller/DateBindHandler.java
+++ src/main/java/com/southwind/controller/DateBindHandler.java
@@ -2,9 +2,12 @@ package com.southwind.controller;
 
 import javax.xml.ws.RespectBinding;
 
+import org.apache.catalina.tribes.util.Arrays;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 /**
  * 
 * <p>Title: DateBindHandler</p>  
@@ -12,21 +15,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 * @author yewenbo
 * @date 2019年11月29日
  */
-@Controller
+@RestController
 @RequestMapping("/data")
 public class DateBindHandler {
 	
 	//基本数字类型
-	@RequestMapping("/baseTyepe")
-	@ResponseBody
+	@RequestMapping("/baseType")
+	//@ResponseBody
 	public String baseType(int id) {
 		return id+"";
 	}
 	
 	//包装类
-	@ResponseBody
-	@RequestMapping("/PackageTyepe")
-	public String PackageTyepe(Integer id) {
+	//@ResponseBody
+	@RequestMapping("/PackageType")
+	public String PackageTyepe(@RequestParam(value = "num" ,required = false,defaultValue="0")Integer id) {
 		return id+"";
+	}
+	//@ResponseBody
+	@RequestMapping("/array")
+	public String array(String[] name) {
+		String str = Arrays.toString(name);
+		return str;
 	}
 }
